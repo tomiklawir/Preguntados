2a5b7d2ea53c79a34b08ba0312ae282e5496dbac
TOMAS DAVID KLAWIR <48385206@ort>
2024-08-26 08:01:50 -0300

    Proyecto creado

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Preguntados.Models;
+
+namespace Preguntados.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
